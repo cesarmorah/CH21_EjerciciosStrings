218bd0f062d8c53de5d5af39d41a872036783142
cesarmora <cesarmorahh@gmail.com>
2022-12-12 16:08:54 -0600

    se agrega funcion de palindromo de forma arreglo y simple

--- invertir/main.js
+++ invertir/main.js
@@ -4,4 +4,25 @@ function invertirCadena(cad) {
     else
       return invertirCadena(cad.substr(1)) + cad.charAt(0);
   }
-  invertirCadena("laura aline");
+  invertirCadena("laura aline");   
+
+  function palindrome(str) {
+    let re = /[\W_]/g;
+    let lowRegStr = str.toLowerCase().replace(re, '');
+    let reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
+  }
+  palindrome("anita lava la tina");
+
+
+  function espalindromo(str){
+
+      str =str.toLowerCase();
+    
+    str = str.replaceAll("", "");
+    
+    if (reves(str)==str) return true;
+    
+    return false;
+    
+    }//esPalindromo I
