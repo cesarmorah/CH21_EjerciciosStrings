7d0617a8c10f3e92a04236f6c8a0234cce123e60
cesarmora <cesarmorahh@gmail.com>
2022-12-12 13:57:55 -0600

    se agrega codigo mostrado por jc

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,31 @@ let cadena = "Aczino se convirtió en el tricampeón de la Red Bull Batalla Inte
 
 let indices = [];
 for(let i = 0; i < cadena.length; i++) {
-	if (cadena[i].toLowerCase() === "a") indices.push(i);
+	if (cadena[i].toLowerCase() === "a  ") indices.push(i);
 }
 
 document.getElementById("resultado").innerHTML = indices.length;
+
+
+//Aquí se escribiran los ejercicios con Strings
+
+let nombre = "abraham"; //"Abraham"; nombre = nombre.toUpperCase();
+let letra="a";
+
+let contFinal=0;
+
+for (let index = 0; index < nombre.length; index++) { console.log(nombre.charAt(index));
+
+let contTemp=1;
+
+for (let cont = index+1; cont < nombre.length; cont++) { if(nombre.charAt(index) == nombre.charAt(cont)) contTemp++;
+
+} // for cont
+
+if (contTemp>contFinal) { contFinal=contTemp; letra = nombre.charAt(index);
+
+//contTemp>contFinal
+
+}// for index
+
+console.log(letra, contFinal);
